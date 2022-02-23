10cc816b3dd80e1894d1479b50845910b5d5b8cc
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-02-23 13:58:52 +0900

    String array copy

---
+++ C_Inflearn/string_duplicate.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * 문제 정의
+ * 입력받은 문자열을 배열에 담아 그대로 출력하기
+ * ex)
+ *     input :
+ *     aaa bbb ccc dd eeee
+ *
+ *     output :
+ *     aaa bbb ccc dd eeee
+ */
+
+
+int main() {
+
+    int n;
+
+    char *words[100];
+
+    printf("입력할 문자열의 수를 정해주세요 (100 이하) : ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        char userInput[100];
+        printf("100자 이하의 문자열을 입력하세요: ");
+        scanf("%s", userInput);
+        //words[i]의 타입     : char*
+        //userInput의 타입 : char[]
+
+        //words[i] = userInput; <- 주소값을 공유 not value
+
+        char *tmp;
+        tmp = (char *) malloc(strlen(userInput) + 1);
+        strcpy(tmp, userInput);
+
+        words[i] = tmp;
+
+        //words[i] = strdup(userInput);
+
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%s\n", *(words + i));
+    }
+
+}
