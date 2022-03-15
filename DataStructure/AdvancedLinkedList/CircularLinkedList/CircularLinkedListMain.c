cefa99c571d4f129601a60626e67eb913312f5a2
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-03-15 16:02:01 +0900

    CircluarLinkedList

---
+++ DataStructure/AdvancedLinkedList/CircularLinkedList/CircularLinkedListMain.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "CircularLinkedList.h"
+
+/**
+ * To Run this Source,
+ * copy following texts and paste to CMakeLists.txt
+
+   add_executable(
+        DataStructure
+        AdvancedLinkedList/CircularLinkedList/CircularLinkedList.h
+        AdvancedLinkedList/CircularLinkedList/CircularLinkedList.c
+        AdvancedLinkedList/CircularLinkedList/CircularLinkedListMain.c
+    )
+
+ */
+
+int main() {
+    List list;
+    int data, i, numNode;
+    ListInit(&list);
+
+    LInsert(&list, 3);
+    LInsert(&list, 4);
+    LInsert(&list, 5);
+    LInsertFront(&list, 2);
+    LInsertFront(&list, 1);
+
+    if (LFirst(&list, &data)) {
+        printf("%d ", data);
+
+        for (i = 0; i < LCount(&list) * 3 - 1; i++) {
+            if (LNext(&list, &data))
+                printf("%d ", data);
+        }
+    }
+    printf("\n");
+
+    numNode = LCount(&list);
+
+    if (numNode != 0) {
+        LFirst(&list, &data);
+        if (data % 2 == 0)
+            LRemove(&list);
+
+        for (i = 0; i < numNode - 1; i++) {
+            LNext(&list, &data);
+            if (data % 2 == 0)
+                LRemove(&list);
+        }
+    }
+
+    if (LFirst(&list, &data)) {
+        printf("%d ", data);
+
+        for (i = 0; i < LCount(&list) - 1; i++) {
+            if (LNext(&list, &data))
+                printf("%d ", data);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
