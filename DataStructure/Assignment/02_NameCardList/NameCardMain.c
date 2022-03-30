58d85a0d49fd69d5254b0f9c7713fb4384f5fc07
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-03-30 21:11:04 +0900

    fix(Assignment 02) : get inputs at every steps

--- DataStructure/Assignment/02_NameCardList/NameCardMain.c
+++ DataStructure/Assignment/02_NameCardList/NameCardMain.c
@@ -22,46 +22,40 @@ int main() {
     ListInit(&nameCards);
 
     int status;
-    char *nameCardName;
-    char *phoneNum;
+    char nameCardName[NAME_LEN];
+    char phoneNum[PHONE_LEN];
 
-    //입력 받지 않는 3, 4번의 요구사항을 충족하기 위한 기본 데이터 추가
-    //3번(전화번호 변경) 데이터
-    nameCard = MakeNameCard("Kongstar", "010-8999-1234");
-    LInsert(&nameCards, nameCard);
-    //4번(특정인 정보 삭제) 데이터
-    nameCard = MakeNameCard("Royce", "010-2241-4348");
-    LInsert(&nameCards, nameCard);
-
+    printf("===================== 명함 입력 ======================\n");
     //1.저장할 명함 항목 입력
     //0을 입력 하면 종료
     while (1) {
         printf("명함의 이름을 입력해 주세요 : ");
-        scanf("%s", &nameCardName);
-        getchar();
+        scanf("%s", nameCardName);
+
         printf("전화번호를 입력해 주세요 : ");
-        scanf("%s", &phoneNum);
-        getchar();
+        scanf("%s", phoneNum);
 
         nameCard = MakeNameCard(&nameCardName, &phoneNum);
         LInsert(&nameCards, nameCard);
 
-        printf("저장 완료!\n");
+        printf("저장 완료!\n\n");
         printf("추가로 입력 하시려면 1, 모두 입력 하셨다면 0을 입력 해주세요 : ");
 
 
         scanf("%d", &status);
         if (status != 1) {
-            printf("===================== 입력 종료 ======================\n\n\n");
+            printf("======================= 종료 =======================\n\n\n");
             break;
         }
     }
 
     //2. 특정인 정보 출력
-    char *findName;
-    findName = "Royce";
-    printf("이름이 %s인 사람의 NameCard 정보\n", findName);
+    printf("===================== 명함 조회 ======================\n");
+    char findName[NAME_LEN];
+    printf("조회하실 명함의 이름을 입력해주세요 : ");
+    scanf("%s", findName);
 
+    printf("이름이 %s인 사람의 명함 정보\n", findName);
     if (LFirst(&nameCards, &nameCard)) {
         if (!NameCompare(nameCard, findName)) {
             ShowNameCardInfo(nameCard);
@@ -74,36 +68,43 @@ int main() {
             }
         }
     }
-    printf("\n\n");
+    printf("======================= 종료 =======================\n\n\n");
+
+    printf("===================== 번호 변경 ======================\n");
 
     //3. 특정인 전화번호 변경
     //"Kongstar"의 전화번호를 changePhone으로 변경
-    char *changeName;
-    char *changePhone;
-    changeName = "Kongstar";
-    changePhone = "010-0000-0000";
+    char changeName[NAME_LEN];
+    char changePhone[PHONE_LEN];
+    printf("전화번호를 변경하실 분의 이름을 입력해주세요 : ");
+    scanf("%s", changeName);
+
+    printf("변경하실 번호를 입력해주세요 : ");
+    scanf("%s", changePhone);
 
     if (LFirst(&nameCards, &nameCard)) {
         if (!NameCompare(nameCard, changeName)) {
             printf("%s의 전화번호를 변경하였습니다\n", changeName);
-            printf("[%s] -> [%s]\n\n\n", nameCard->phone, changePhone);
+            printf("[%s] -> [%s]\n", nameCard->phone, changePhone);
             ChangePhoneNum(nameCard, changePhone);
         } else {
             while (LNext(&nameCards, &nameCard)) {
                 if (!NameCompare(nameCard, changeName)) {
                     printf("%s의 전화번호를 변경하였습니다\n", changeName);
-                    printf("[%s] -> [%s]\n\n\n", nameCard->phone, changePhone);
+                    printf("[%s] -> [%s]\n", nameCard->phone, changePhone);
                     ChangePhoneNum(nameCard, changePhone);
                     break;
                 }
             }
         }
     }
-    printf("\n");
+    printf("======================= 종료 =======================\n\n\n");
 
     //4. "Royce"의 정보 삭제
-    char *removeName;
-    removeName = "Royce";
+    printf("===================== 명함 삭제 ======================\n");
+    char removeName[NAME_LEN];
+    printf("삭제할 명함의 이름을 입력해주세요 : ");
+    scanf("%s", removeName);
     if (LFirst(&nameCards, &nameCard)) {
         if (!NameCompare(nameCard, removeName)) {
             nameCard = LRemove(&nameCards);
@@ -119,7 +120,8 @@ int main() {
         }
     }
 
-    printf("%s의 정보가 삭제되었습니다\n\n\n", removeName);
+    printf("%s의 정보가 삭제되었습니다\n", removeName);
+    printf("======================= 종료 =======================\n\n\n");
 
     //5. 저장되어 있는 명함 정보 출력
     printf("=================== 명함 목록 출력 ====================\n");
