c4e0c11d77bb38b5f91e6512d666b3bf75f50263
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-04-01 22:44:01 +0900

    Deque

---
+++ DataStructure/Queue/Deque/Deque.h
@@ -0,0 +1,36 @@
+#ifndef DATASTRUCTURE_DEQUE_H
+#define DATASTRUCTURE_DEQUE_H
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int Data;
+
+typedef struct _node {
+    Data data;
+    struct _node *next;
+    struct _node *prev;
+} Node;
+
+typedef struct _dlDeque {
+    Node *head;
+    Node *tail;
+} DLDeque;
+
+typedef DLDeque Deque;
+
+void DequeInit(Deque *pdeq);
+
+int DQIsEmpty(Deque *pdeq);
+
+void DQAddFirst(Deque *pdep, Data data);
+void DQAddLast(Deque *pdeq, Data data);
+
+Data DQRemoveFirst(Deque *pdeq);
+Data DQRemoveLast(Deque *pdep);
+
+//peek은 데이터를 참조
+Data DQPeekFirst(Deque *pdeq);
+Data DQPeekLast(Deque *pdeq);
+
+#endif //DATASTRUCTURE_DEQUE_H
