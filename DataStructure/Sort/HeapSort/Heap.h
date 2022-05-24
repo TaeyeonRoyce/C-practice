de19fd8a24bf9996429f9b2f6ac2c849bde9c3f2
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-05-24 14:18:46 +0900

    DataStucture Advanced Sort

---
+++ DataStructure/Sort/HeapSort/Heap.h
@@ -0,0 +1,25 @@
+#ifndef DATASTRUCTURE_HEAP_H
+#define DATASTRUCTURE_HEAP_H
+
+
+#define TRUE 1
+#define FALSE 0
+
+#define HEAP_LEN 100
+
+typedef int HData;
+typedef int PriorityComp(HData data1, HData data2);
+
+typedef struct _heap {
+    PriorityComp *comp;
+    int numOfData;
+    HData heapArr[HEAP_LEN];
+} Heap;
+
+void HeapInit(Heap *ph, PriorityComp pc);
+int HIsEmpty(Heap *ph);
+
+void HInsert(Heap *ph, HData data);
+HData HDelete(Heap *ph);
+
+#endif //DATASTRUCTURE_HEAP_H
