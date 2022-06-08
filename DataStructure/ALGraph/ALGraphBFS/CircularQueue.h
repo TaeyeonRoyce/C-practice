c0aca770d112afcb1870052894e9b622e2eed587
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-06-08 17:10:07 +0900

    DataStucture ALGraph BFS Search

---
+++ DataStructure/ALGraph/ALGraphBFS/CircularQueue.h
@@ -0,0 +1,30 @@
+#ifndef DATASTRUCTURE_CIRCULARQUEUE_H
+#define DATASTRUCTURE_CIRCULARQUEUE_H
+
+#ifndef __C_QUEUE_H__
+#define __C_QUEUE_H__
+
+#define TRUE 1
+#define FALSE 0
+
+#define QUE_LEN 100
+typedef int Data;
+
+typedef struct _cQueue {
+    int front;
+    int rear;
+    Data queArr[QUE_LEN];
+} CQueue;
+
+typedef CQueue Queue;
+
+void QueueInit(Queue *pq);
+int QIsEmpty(Queue *pq);
+
+void Enqueue(Queue *pq, Data data);
+Data Dequeue(Queue *pq);
+Data QPeek(Queue *pq);
+
+#endif
+
+#endif //DATASTRUCTURE_CIRCULARQUEUE_H
